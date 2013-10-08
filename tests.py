a6b6954eafb51cd821a32f66146d8cb684dca7b4
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2013-10-08 17:09:20 -0500

    Added model and tests for users.

--- tests.py
+++ tests.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 
 import unittest
+import webapp
 
 class TestHello(unittest.TestCase):
   def setUp(self):
-    from webapp import app
-    self.client = app.test_client()
+    webapp.app.config["TESTING"] = True
+    # Tell SQLAlchemy to use an in-memory database.
+    webapp.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+    self.client = webapp.app.test_client()
+    webapp.db.create_all()
+
+  def tearDown(self):
+    webapp.db.drop_all()
 
   def test_hello(self):
     self.assertTrue("<h1>Hello!</h1>" in self.client.get("/hello").data)
+
+  def test_db_setup(self):
+    user = webapp.User(login="foo", email="foo@gmail.com", password="password")
+    webapp.db.session.add(user)
+    webapp.db.session.commit()
 
 if __name__ == "__main__": unittest.main()
 
--- webapp.py
+++ webapp.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
 from flask import Flask, render_template
-
+from flask.ext.sqlalchemy import SQLAlchemy
+ 
+SQLALCHEMY_DATABASE_URI = "sqlite:///hello.db"
+  
 app = Flask(__name__)
+app.config.from_object(__name__)
+db = SQLAlchemy(app)
+
+class User(db.Model):
+  __tablename__ = "users"
+  id = db.Column(db.Integer, primary_key=True)
+  login = db.Column(db.String, unique=True, nullable=False)
+  email = db.Column(db.String, unique=True, nullable=False)
+  password = db.Column(db.String, nullable=False)
 
 @app.route("/hello")
 def hello():
