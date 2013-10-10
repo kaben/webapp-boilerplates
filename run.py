755bcb4cc1a7fbf51de578905f213411d4f58877
Kaben Nanlohy <kaben.nanlohy@gmail.com>
2013-10-09 23:15:39 -0500

    Moved all files into new subdirectory 'flask-boilerplate'.

--- run.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-from webapp import app
-
-# If this script is being executed instead of imported, run the webapp.
-if __name__ == "__main__": app.run(debug=True)
-
--- view_db.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import webapp
-
-def view_database(app):
-  return "\n".join(repr(user) for user in app.db.session.query(app.User))
-
-if __name__ == "__main__": print view_database(webapp)
-
